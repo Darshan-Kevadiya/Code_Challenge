69e111e0c6edda85e82b9465e10e16f09a08e7d4
Darshan Kevadiya <darshankevadiya019@gmail.com>
2022-09-26 09:31:10 +0530

    Pagination

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts';
+import { Button } from 'react-bootstrap';
 
 const Users = () => {
   const chart2Options = {
@@ -24,7 +25,7 @@ const Users = () => {
         enabled: true
       },
       x: {
-        show: false
+        show: true
       },
       y: {
         title: {
@@ -54,43 +55,89 @@ const Users = () => {
 
   const [data, setdata] = useState([])
   const [logsData, setLogsData] = useState([])
-
+  const [enteries, setenteries] = useState(10)
+  const [response, setresponse] = useState('')
+  const [offset, setoffset] = useState('')
+  const [offsetarray, setoffsetarray] = useState([])
+  
+  
   useEffect(() => {
-    axios.get('https://api.airtable.com/v0/appBTaX8XIvvr6zEC/Users?maxRecords=12',
+    axios.get(`https://api.airtable.com/v0/appBTaX8XIvvr6zEC/Users?pageSize=${enteries}${offset && `&offset=${offset}`}`,
       {
         headers: {
           'Authorization': 'Bearer key4v56MUqVr9sNJv'
         }
       }
-    ).then(x => setdata(x.data.records))
+    ).then(x => {
+      setdata(x.data.records);
+      console.log(x.data.offset);
+      setresponse(x.data)
+      !x.data.offset && offsetarray.pop()
+    })
 
-    //  
+    
     axios.get('/assets/logs.json').then(({ data }) => {
       setLogsData(data)
     })
-  }, [])
 
-  console.log(data);
+  }, [enteries , offset])
+
+  const handleEnteries =(e) =>{
+    setenteries(e.target.value);
+  }
+  const handlenext = () =>{
+    console.log(response.offset);
+    setoffset(response.offset)
+    setoffsetarray(prevstate => ([...prevstate , response.offset]))
+  }
+  const handleprevious = () =>{
+    setoffset(offsetarray.length > 0 ? offsetarray.pop() : ' ')
+    offsetarray.length == 0 &&  setoffset('')
+  }
+
+  // console.log(data);
+  // console.log("response offset" ,response.offset);
+  // console.log("offset array",offsetarray);
+  // console.log("offset array",offsetarray.length);
+  // console.log("offset",offset);
+
+  
   return (
     <div>
+      <div className='container d-flex justify-content-between my-2'>
+        <div className='d-flex align-items-center'>
+       <h5 className='m-0'>Show</h5>
+        <select class="form-select mx-2" onChange={handleEnteries}>
+          <option value='10'>10</option>
+          <option value='25'>25</option>
+          <option value='50'>50</option>
+          <option value='100'>100</option>
+        </select>
+        <h5 className='m-0'>enteries</h5>
+        </div>
+        <div>
+          <Button onClick={handleprevious} disabled={offsetarray.length > 0 ? false : true}>Previous</Button>
+          <Button onClick={handlenext} disabled={response.offset ? false : true}>Next</Button>
+        </div>
+      </div>
       <div className='container d-flex row g-0 mx-auto'>
         {
           data.map(x => {
-            return <div className='col-4 p-2'>
-              <div className=' p-2 d-flex flex-column card'>
-                <div className='d-flex w-100'>
+            return <div className=' col-12 col-sm-6 col-xl-4 p-2 card-group'>
+              <div className=' p-2 d-flex flex-column card justify-content-between'>
+                <div className='d-flex flex-sm-column align-items-center align-items-md-start flex-md-row w-100'>
                   <div className='col-3' >
                     <div style={{ width: '70px', height: '70px', borderRadius: '50%', border: '2px solid', color: 'white', background: 'rgb(127, 140, 141)' }} className='d-flex justify-content-center align-items-center fs-3'>
                       <span>{Firstletter(x.fields.Name)}</span>
                     </div>
                   </div>
-                  <div className='user-name col-9 d-flex flex-column text-start'>
-                    <h4 className='m-0 heading-color fw-bolder'>{x.fields.Name}</h4>
-                    <p className='m-0 text-muted fw-bolder'>{x.fields.occupation}</p>
+                  <div className='user-name col-9 col-sm-12 col-md-9 d-flex flex-column justify-content-center h-100 text-start ps-1'>
+                    <h4 className='m-0 heading-color fw-bolder text-sm-center text-md-start'>{x.fields.Name}</h4>
+                    <p className='m-0 text-muted fw-bolder text-sm-center text-md-start'>{x.fields.occupation}</p>
                   </div>
                 </div>
                 <div className='d-flex'>
-                  <div className='col-7 d-flex flex-column justify-content-between'>
+                  <div className='col-6 col-lg-7 d-flex flex-column justify-content-between'>
                     <div>
                       <Chart
                         options={chart2Options}
@@ -101,12 +148,12 @@ const Users = () => {
                     </div>
                     <span className='text-muted' style={{ fontSize: '12px' }}>Conversion 4/12 - 4/30</span>
                   </div>
-                  <div className='col-5 text-end'>
-                    <h6 className='m-0 orange-color fw-bold fs-5'>{ parseFloat(logsData.filter(y => y.type == 'impression' && y.user_id == x.fields.Id).map(y => y.revenue).reduce((a, b) => a + b, 0)).toFixed(2) }</h6>
+                  <div className='col-6 col-lg-5 text-end'>
+                    <h6 className='m-0 orange-color fw-bold fs-6 fs-lg-5'>{ parseFloat(logsData.filter(y => y.type == 'impression' && y.user_id == x.fields.Id).map(y => y.revenue).reduce((a, b) => a + b, 0)).toFixed(2) }</h6>
                     <span className='m-0 text-muted' style={{ fontSize: '14px' }}>impressions  </span>
-                    <h6 className='m-0 blue-color fw-bold fs-5'>{  parseFloat(logsData.filter(y => y.type == 'conversion' && y.user_id == x.fields.Id).map(y => y.revenue).reduce((a, b) => a + b, 0)).toFixed(2) }</h6>
+                    <h6 className='m-0 blue-color fw-bold fs-6 fs-lg-5'>{  parseFloat(logsData.filter(y => y.type == 'conversion' && y.user_id == x.fields.Id).map(y => y.revenue).reduce((a, b) => a + b, 0)).toFixed(2) }</h6>
                     <span className='m-0 text-muted' style={{ fontSize: '14px' }}>conversion  </span>
-                    <h6 className='m-0 green-color fw-bold fs-4'>${ parseFloat(logsData.filter(y => y.user_id == x.fields.Id).map(y => y.revenue).reduce((a, b) => a + b, 0)).toFixed(2) }</h6>
+                    <h6 className='m-0 green-color fw-bold fs-5 fs-lg-4'>${ parseFloat(logsData.filter(y => y.user_id == x.fields.Id).map(y => y.revenue).reduce((a, b) => a + b, 0)).toFixed(2) }</h6>
                   </div>
                 </div>
               </div>
